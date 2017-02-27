046de5b681945751c41a20abd5429b8300412f9f
Pierre-Luc Carmel Biron <plbc@hotmail.com>
2017-02-27 16:21:26 -0500

    Row expansion using the spacebar

--- addon/behaviors/row-expansion.js
+++ addon/behaviors/row-expansion.js
@@ -1,4 +1,5 @@
 import Behavior from 'ember-light-table/behaviors/behavior';
+import { keyDown } from 'ember-keyboard';
 
 export default Behavior.extend({
 
@@ -10,17 +11,28 @@ export default Behavior.extend({
 
   init() {
     this._super(...arguments);
-    this.events.onToggleExpandedRow = ['rowClick:_none'];
+    this.events.onToggleClick = ['rowClick:_none'];
+    this.events.onToggleFocused = [keyDown('Space')];
   },
 
-  onToggleExpandedRow(ltBody, ltRow) {
-    let row = ltRow.get('row');
+  _onToggle(ltBody, row) {
+    let shouldExpand = !row.get('expanded');
+    if (!this.get('multiRow')) {
+      ltBody.get('table.expandedRows').setEach('expanded', false);
+    }
+    row.set('expanded', shouldExpand);
+  },
+
+  onToggleClick(ltBody, ltRow) {
     if (this.get('expandOnClick')) {
-      let shouldExpand = !row.get('expanded');
-      if (!this.get('multiRow')) {
-        ltBody.get('table.expandedRows').setEach('expanded', false);
-      }
-      row.set('expanded', shouldExpand);
+      this._onToggle(ltBody, ltRow.get('row'));
+    }
+  },
+
+  onToggleFocused(ltBody) {
+    let focusedRow = ltBody.get('table.focusedRow');
+    if (focusedRow) {
+      this._onToggle(ltBody, focusedRow);
     }
   }
 

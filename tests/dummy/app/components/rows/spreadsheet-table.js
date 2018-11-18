0f55413d881266d44c4bb50eec8b9cd6736d69fa
Pierre-Luc Carmel Biron <plbc@hotmail.com>
2018-11-18 10:00:27 -0500

    Update spreadsheet-table in demo app.

--- tests/dummy/app/components/rows/spreadsheet-table.js
+++ tests/dummy/app/components/rows/spreadsheet-table.js
@@ -1,9 +1,10 @@
-// BEGIN-SNIPPET selectable-table
+// BEGIN-SNIPPET spreadsheet-table
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import TableCommon from '../../mixins/table-common';
 
 export default Component.extend(TableCommon, {
+
   hasSelection: computed.notEmpty('table.selectedRows'),
 
   columns: computed(function() {

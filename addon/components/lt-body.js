ccf79397e83d9106799a674513eb8938091c2934
Pierre-Luc Carmel Biron <plbc@hotmail.com>
2020-12-07 21:16:41 -0500

    Rename ember-behavior to ember-behaviors

--- addon/components/lt-body.js
+++ addon/components/lt-body.js
@@ -8,7 +8,7 @@ import { warn } from '@ember/debug';
 import layout from 'ember-light-table/templates/components/lt-body';
 import { EKMixin } from 'ember-keyboard';
 import ActivateKeyboardOnFocusMixin from 'ember-keyboard/mixins/activate-keyboard-on-focus';
-import { HasBehaviorsMixin, behaviorGroupFlag, behaviorFlag, behaviorInstanceOf } from 'ember-behavior';
+import { HasBehaviorsMixin, behaviorGroupFlag, behaviorFlag, behaviorInstanceOf } from 'ember-behaviors';
 import RowExpansionBehavior from 'ember-light-table/behaviors/row-expansion';
 import SingleSelectBehavior from 'ember-light-table/behaviors/single-select';
 import MultiSelectBehavior from 'ember-light-table/behaviors/multi-select';
